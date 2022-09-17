6e8d192ebfcaa707cf95f8b1fdef7ad680d467b3
FosterClark48 <fosterclark48@yahoo.com>
2022-09-16 17:25:09 -0700

    C program printing size of var types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,2 +1,22 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ * main - prints size of var types
+ * return: always 0
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
+	/* sizeof evaluates the size of a variable */
+	printf("Size of char: %d byte(s)\n", sizeof(a));
+	printf("Size of int: %d byte(s)\n", sizeof(b));
+	printf("Size of long int: %d byte(s)\n", sizeof(c));
+	printf("Size of long long int: %d byte(s)\n", sizeof(d));
+	printf ("Size of float: %d byte(s)\n", sizeof(e));
+
+	return (0);
+}
