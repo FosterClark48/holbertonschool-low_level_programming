8a87eaa9971d69a67f39dcba1a3355a59fafdd03
FosterClark48 <fosterclark48@yahoo.com>
2022-09-16 17:30:46 -0700

    C program printing size of var types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 
 	/* sizeof evaluates the size of a variable */
 	printf("Size of a char: %ld byte(s)\n", sizeof(a));
-	printf("Size of a int: %ld byte(s)\n", sizeof(b));
+	printf("Size of an int: %ld byte(s)\n", sizeof(b));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
 	printf("Size of a float: %ld byte(s)\n", sizeof(e));
