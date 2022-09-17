26351e3fed264b4ce6ed169abeaed60adaa7d170
FosterClark48 <fosterclark48@yahoo.com>
2022-09-16 17:00:21 -0700

    C program that prints using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
