1f41b25345cacdffe8ea3333b064dece09f0f392
FosterClark48 <fosterclark48@yahoo.com>
2022-09-18 22:32:45 -0700

    Program that prints all single digit numbers of base 10 using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,1 +1,18 @@
 #include <stdio.h>
+/**
+*main - Entry Function
+*
+*Return: 0 - Standard Return
+*/
+int main(void)
+{
+	int base10;
+
+	for (base10 = 0; base10 < 10; base10++)
+	{
+		putchar((base10 % 10) + '0');
+
+	}
+	putchar('\n');
+	return (0);
+}
