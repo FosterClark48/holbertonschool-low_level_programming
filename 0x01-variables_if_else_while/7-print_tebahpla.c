75161dec06fe601d06c9e43e133e9a27fe6c5c9e
FosterClark48 <fosterclark48@yahoo.com>
2022-09-18 22:36:32 -0700

    Program that prints alphabet backwards

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,17 @@
 #include <stdio.h>
+/**
+*main - Entry Function
+*
+*Return: 0 - Standard Return
+*/
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
