78abb509ff287681db7c405e35666b3387c46e8f
FosterClark48 <fosterclark48@yahoo.com>
2022-09-18 22:41:09 -0700

    Program that prints no. of base 16 in lc

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,22 @@
 #include <stdio.h>
+/**
+*main - Entry function
+*
+*Return: 0 - Standard Return
+*/
+int main(void)
+{
+	int base16;
+	char alpha;
+
+	for (base16 = 0; base16 < 10; base16++)
+	{
+		putchar((base16 % 10) + '0');
+	}
+	for (alpha = 'a'; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
