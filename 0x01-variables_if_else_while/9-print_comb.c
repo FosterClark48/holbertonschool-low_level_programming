5060b9eb39a85cae4ca2017cb4e497b9c9a010b7
FosterClark48 <fosterclark48@yahoo.com>
2022-09-18 22:44:34 -0700

    Program that prints all possible combos of single digit no

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+/**
+*main - Entry Function
+*
+*Return: 0 - Standard Return
+*/
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar((num % 10) + '0');
+		{
+			putchar(44);
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
