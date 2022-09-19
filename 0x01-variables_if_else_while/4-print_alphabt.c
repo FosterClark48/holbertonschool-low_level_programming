809288c03115d1795282c447e9e03cc7cc23f56f
FosterClark48 <fosterclark48@yahoo.com>
2022-09-18 22:24:23 -0700

    Program that prints alphabet except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+/**
+*main - Entry Function
+*
+*Return: 0 - Standard Return
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		{
+			putchar(ch);
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
