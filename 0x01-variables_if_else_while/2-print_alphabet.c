95e02a38a6b3b5e15de34361bea788138199ee0c
FosterClark48 <fosterclark48@yahoo.com>
2022-09-18 21:55:28 -0700

    Program that prints alphabet in lc

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+/**
+*main - Entry Function
+*
+*Return: 0-Standard Return
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+		putchar('\n');
+
+	return (0);
+}
