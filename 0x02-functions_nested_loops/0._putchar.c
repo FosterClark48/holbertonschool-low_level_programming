f3c6d13bc24bb0218d13af0b93587b9374d39a2c
FosterClark48 <fosterclark48@yahoo.com>
2022-09-20 19:38:11 -0700

    Program that prints _putchar using putchar

---
+++ 0x02-functions_nested_loops/0._putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*main - Entry Function
+*
+*Return: 0 - Standard Return
+*/
+
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Learning about C Functions, & Nested Loops
