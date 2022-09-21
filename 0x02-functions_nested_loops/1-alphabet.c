1d7ff59e9e5509039aca41799bf62f2ae5677f0d
FosterClark48 <fosterclark48@yahoo.com>
2022-09-20 20:44:11 -0700

    Prints the alphabet in lc

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*print_alphabet - prints the alphabet
+*
+*Return: 0 - True
+*/
+void print_alphabet(void)
+{
+	char i;
+
+	for (i =  'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+
