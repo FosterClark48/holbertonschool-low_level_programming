9cd602e5c412232222b630491c627e0ce46ad570
FosterClark48 <fosterclark48@yahoo.com>
2022-09-20 21:42:13 -0700

    Prints every min of day of jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every min of day
+ * Description: prints every min of day
+ * Return: Void
+ */
+
+void jack_bauer(void)
+{
+		int a, b, c, d;
+
+		for (a = 48; a <= 50; a++)
+		{
+		for (b = 48; b <= 57; b++)
+		{
+		for (c = 48; c <= 53; c++)
+		{
+		for (d = 48; d <= 57; d++)
+		{
+		if (a >= 50 && b >= 52)
+		break;
+		_putchar(a);
+		_putchar(b);
+		_putchar(58);
+		_putchar(c);
+		_putchar(d);
+		_putchar('\n');
+		}
+		}
+	}
+}
+}
