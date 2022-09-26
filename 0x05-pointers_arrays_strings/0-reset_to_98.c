cbcf2f79ff1b801c79b53b05f854ca1db446642f
FosterClark48 <fosterclark48@yahoo.com>
2022-09-26 08:29:25 -0700

    First push with task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+*reset_to_98 - Updates value to 98
+*@n: value of int
+*Description: takes value and updates to 98
+*Return: 0 = Success
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
