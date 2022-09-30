bd6e4d371624d75f67e722eceff49b61fa3d0da9
FosterClark48 <fosterclark48@yahoo.com>
2022-09-30 15:01:24 -0700

    Write your own toupper function

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+*string_toupper - function changing lowercase to uppercase
+*@*: pointer variable?
+*
+*Return 0 = Success
+*/
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
