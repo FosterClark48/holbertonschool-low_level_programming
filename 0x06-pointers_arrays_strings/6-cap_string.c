d162eab8e5d717b6afe406a49791408412919ed4
FosterClark48 <fosterclark48@yahoo.com>
2022-10-02 17:59:08 -0700

    Function that caps first letter of words after spcial char in a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,25 +15,21 @@ char *cap_string(char *str)
 		{
 			if ((str[i] >= 'a' && str[i] <= 'z'))
 				str[i] = str[i] - 32;
-			continue;
 		}
 		if (str[i] == ' ' || str[i] == ',' || str[i] == ';' || str[i] == '.'
 			|| str[i] == '!' || str[i] == '?' || str[i] == '"'
 			|| str[i] == '(' || str[i] == ')' || str[i] == '{'
 			|| str[i] == '}' || str[i] == '\t' || str[i] == '\n')
 		{
-			++i;
-			if (str[i] >= 'a' && str[i] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
 			{
-				str[i] = str[i] - 32;
-				continue;
+				str[i + 1] = str[i + 1] - 32;
 			}
+			else
+				continue;
 		}
 		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-				str[i] = str[i] + 32;
-		}
+			continue;
 	}
 	return (str);
 }
