f19fbd135ab758769b641a11ad7a2f028ab99417
FosterClark48 <fosterclark48@yahoo.com>
2022-10-11 14:08:03 -0700

    Program that adds positive numbers only

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+/**
+*main - Entry function
+*@argc: count of args supplied to prog/size of argv array
+*@argv: array of pointers to strings which are the args
+*Return: 0 = Success
+*/
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+	int sum = 0;
+
+	for (i = 1; argc > 1; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
