80be3dc0fb8e90804d7577fa7acfac313b1a8180
FosterClark48 <fosterclark48@yahoo.com>
2022-10-24 18:42:21 -0700

    Write a function that prints Struct Dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,6 +12,6 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", (*d).name);
 		printf("Age: %f\n", (*d).age);
-		printf("Owner: %s\n", (*d).name);
+		printf("Owner: %s\n", (*d).owner);
 	}
 }
