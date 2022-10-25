5f19a9707aecc787b2b5937b7735d734dbda8722
FosterClark48 <fosterclark48@yahoo.com>
2022-10-25 10:49:41 -0700

    Initial Commit w/ necessary files/directories

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+Learning what Function Pointers are & how to use them.
+What a function pointer holds exactly
+& where a function pointer points to in the virtual memory
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,20 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS-H
+
+/*
+*libraries - included
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+/*
+*Prototypes - prototypes for functions
+*/
+int _putchar(char c);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
+
+#endif
