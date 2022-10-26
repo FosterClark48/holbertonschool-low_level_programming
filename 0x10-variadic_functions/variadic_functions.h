6bd38d3449d1215780c58aed79be73be9f7718d3
FosterClark48 <fosterclark48@yahoo.com>
2022-10-25 21:06:23 -0700

    working on fixes

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,10 +8,6 @@
 #include <stdarg.h>
 
 
-/**
-*Prototypes - function prototypes
-*/
-int _putchar(char);
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
