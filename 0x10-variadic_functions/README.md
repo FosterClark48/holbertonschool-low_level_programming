07b97e54891ae8b4a34b2e7f63a3d291314e7c35
FosterClark48 <fosterclark48@yahoo.com>
2022-10-25 20:27:13 -0700

    Initial Commit

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,4 @@
+Learning about Variadic Functions
+What they are
+How to use va_start, va_arg and va_end macros
+Why and how to use the const type qualifier
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,20 @@
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+
+/**
+*Prototypes - function prototypes
+*/
+int _putchar(char);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
