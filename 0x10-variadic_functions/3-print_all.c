76bfc16f47d8e7d0288c7c1a543777b53a7087d7
FosterClark48 <fosterclark48@yahoo.com>
2022-10-28 12:27:12 -0700

    updated code not using switch cases

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,44 +1,84 @@
 #include "variadic_functions.h"
 /**
+*print_c - prints a char
+*@c: char to print
+*Return: void
+*/
+void print_c(va_list c)
+{
+	printf("%c", va_arg(c, int));
+}
+
+/**
+*print_s - prints a string
+*@s: string to print
+*Return: void
+*/
+void print_s(va_list s)
+{
+	char *str = va_arg(s, char *);
+
+	if (str == NULL)
+		str = "(nil)";
+	printf("%s", str);
+}
+
+/**
+*print_i - prints an int
+*@i: int to print
+*Return: void
+*/
+void print_i(va_list i)
+{
+	printf("%d", va_arg(i, int));
+}
+
+/**
+*print_f - prints a float
+*@f: float to print
+*Return: void
+*/
+void print_f(va_list f)
+{
+	printf("%f", va_arg(f, double));
+}
+
+/**
 *print_all - Prints anything
 *@format: list of arg types passed to function
 *Return: void
 */
 void print_all(const char * const format, ...)
 {
-	va_list arglist;
-	char p[] = "%_";
-	int i = 0;
+	unsigned int i, j;
+	print_t p[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
+		{NULL, NULL}
+	};
+	va_list valist;
+	char *separator = "";
 
-	va_start(arglist, format);
-	while (format[i])
+	va_start(valist, format);
+	i = 0;
+	while (format && format[i])
 	{
-		if (!(format[i] == 'i' || format[i] == 'c' ||
-			format[i] == 'f' || format[i] == 's'))
+		j = 0;
+		while (p[j].t != NULL)
 		{
-			i++;
-			continue;
-		}
-		if (i != 0)
-		{
-			printf(", ");
-		}
-		p[1] = format[i];
-		switch (format[i])
-		{
-			case 'i':
-			case 'c':
-				printf(p, va_arg(arglist, int));
+			if (*(p[j].t) == format[i])
+			{
+				printf("%s", separator);
+				p[j].f(valist);
+				separator = ", ";
 				break;
-			case 'f':
-				printf(p, va_arg(arglist, double));
-				break;
-			case 's':
-				printf(p, va_arg(arglist, char *));
-				break;
+			}
+			j++;
 		}
 		i++;
 	}
-	va_end(arglist);
+	va_end(valist);
 	printf("\n");
 }
