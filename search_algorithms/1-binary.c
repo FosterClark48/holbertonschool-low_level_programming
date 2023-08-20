dd8f1e489efde1407ab5d960732cb61e39052a5d
Fosterclark48 <fosterclark48@yahoo.com>
2023-08-19 20:44:52 -0700

    Script that uses Binary search algo to search for a value in a sorted array of ints

---
+++ search_algorithms/1-binary.c
@@ -0,0 +1,58 @@
+#include "search_algos.h"
+
+/**
+ * print_array - prints an array of integers
+ * @array: the array to print
+ * @left: index to start printing from
+ *@right: index to stop printing
+ */
+
+void print_array(int *array, size_t left, size_t right)
+{
+	size_t i;
+
+	printf("Searching in array: ");
+	for (i = left; i <= right; i++)
+	{
+		printf("%d", array[i]);
+		if (i < right)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+/**
+ * linear_search - searches for a value in sorted array
+ * of integers using the Binear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Return: first index where value is located or -1 if value is not
+ * present in array or if array is NULL
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t left = 0;
+	size_t right = size - 1;
+	size_t mid;
+
+	if (array == NULL)
+		return (-1);
+
+	while (left <= right)
+	{
+		print_array(array, left, right);
+
+		mid = left + (right - left) / 2;
+		if (array[mid] == value)
+			return ((int)mid);
+		else if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+
+	return (-1);
+}
