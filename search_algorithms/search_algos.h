e5785898027b3ab2c94fd9fcd9a345932d0dcb3a
Fosterclark48 <fosterclark48@yahoo.com>
2023-08-19 20:02:33 -0700

    removed unnecessary items

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -6,22 +6,6 @@
 #include <string.h>
 #include <unistd.h>
 
-/**
- * struct skiplist_search - Singly linked list
- *
- * @n: Integer
- * @index: Index of the node in the list
- * @next: Pointer to the next node
- * @express: Pointer to the next node in the express lane
- */
-typedef struct skiplist_search
-{
-	int n;
-	size_t index;
-	struct skiplist_search *next;
-	struct skiplist_search *express;
-} skiplist_t;
-
 
 int linear_search(int *array, size_t size, int value);
 
